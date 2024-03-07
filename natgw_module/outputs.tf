0d973f3c6ff38fd1888c164f4de4a2e72560ddcc
apahomovs <153571666+apahomovs@users.noreply.github.com>
2024-03-06 19:33:12 -0600

    Update outputs.tf

--- natgw_module/outputs.tf
+++ natgw_module/outputs.tf
@@ -1,3 +1,9 @@
-output "id" {
-  value = aws_nat_gateway.natgw.id
+# outputs.tf
+
+output "natgw_id" {
+  value       = aws_nat_gateway.nat_gateway.id
+}
+
+output "natgw_eip" {
+  value       = aws_eip.nat_eip.id
 }
