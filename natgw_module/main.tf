ea667ea92dc8df3f01bc0932b817762f8205c861
apahomovs <153571666+apahomovs@users.noreply.github.com>
2024-03-06 19:34:21 -0600

    Update main.tf

--- natgw_module/main.tf
+++ natgw_module/main.tf
@@ -2,7 +2,7 @@ resource "aws_eip" "eip" {
   domain = "vpc"
 }
 
-resource "aws_nat_gateway" "natgw" {
+resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.eip.id
   subnet_id = var.subnet_id
   tags = {
